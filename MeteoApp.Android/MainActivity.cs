2d7d1a299d882551416a2ae5330f856e52a93178
cem94 <cem.koca@student.supsi.ch>
2019-04-12 10:19:51 +0200

    * MainActivity.cs: Add permission request
    * Info.plist:
    * MeteoApp.iOS.csproj:
    * MeteoApp.Android.csproj:
    * Resource.designer.cs:

--- MeteoApp.Android/MainActivity.cs
+++ MeteoApp.Android/MainActivity.cs
@@ -14,12 +14,21 @@ namespace MeteoApp.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            // Get the current activity and init
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        // Request permission
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
